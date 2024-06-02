7d26ff97a56745ae1f2032a86137b58f52011951
Fasiku Daniel <daniel.fasiku@stu.cu.edu.ng>
2024-06-02 11:24:39 +0100

    FINAL COMMIT

--- src/data.js
+++ src/data.js
@@ -21,22 +21,22 @@ export const links = [
 
 export const team = [
     {
-        imagePath: "../src/assets/team-img1.svg",
+        imagePath: "../public/team-img1.svg",
         name: "Cornelius Adetutu",
         title: "Cco"
     },
     {
-        imagePath: "../src/assets/team-img2.svg",
+        imagePath: "../public/team-img2.svg",
         name: "Adewale Inioluwa",
         title: "Copywriting/Project Manager"
     },
     {
-        imagePath: "../src/assets/team-img3.svg",
+        imagePath: "../public/team-img3.svg",
         name: "Jegede Toye",
         title: "Chief Designer"
     },
     {
-        imagePath: "../src/assets/team-img4.svg",
+        imagePath: "../public/team-img4.svg",
         name: "Favour Okunola",
         title: "Content Writer"
     },
@@ -45,7 +45,7 @@ export const team = [
 export const services = [
     {
         id: 1,
-        imagePath: "../src/assets/service1.svg",
+        imagePath: "../public/service1.svg",
         title: "Event",
         title2: "Management",
         info: "Planning and strategizing events to ensure their success. This includes understanding the client's goals and objectives, defining the event's purpose, and developing a comprehensive event plan that outlines the timeline, logistics, budget, and desired outcomes"
@@ -54,7 +54,7 @@ export const services = [
 export const services2 = [
     {
         id: 2,
-        imagePath: "../src/assets/service2.svg",
+        imagePath: "../public/service2.svg",
         title: "Social Media",
         title2: "Management",
         info: "Through our incredible content marketing strategy, which employs online guides to drive leads, we help you handle and manage your social media accounts, which results in more traffic and engagement."
@@ -64,7 +64,7 @@ export const services2 = [
 export const services3 = [
     {
         id: 3,
-        imagePath: "../src/assets/service3.svg",
+        imagePath: "../public/service3.svg",
         title: "Advertising",
         title2: "/Branding",
         info: "We create television commercials, radio ads, billboard ads, social media content, and other channels to raise your visibility. In addition, we assist you in creating a solid brand by enhancing your online presence, business culture, product quality, website and marketing, taglines and slogans, and customer service."
@@ -74,7 +74,7 @@ export const services3 = [
 export const services4 = [
     {
         id: 4,
-        imagePath: "../src/assets/service4.svg",
+        imagePath: "../public/service4.svg",
         title: "Influencer/Community",
         title2: "Management",
         info: "We have you covered if you decide to use social media to support your marketing and sales cycle. We'll make use of our social media marketing tools, which make it simple for us to view, classify, and order all incoming messages and conversations centered around your brand."
@@ -84,7 +84,7 @@ export const services4 = [
 export const services5 = [
     {
         id: 5,
-        imagePath: "../src/assets/service5.svg",
+        imagePath: "../public/service5.svg",
         title: "Social Listening",
         title2: "/Analytics",
         info: "We examine the trends and dialogues affecting not just your business but also your industry as a whole, and we use the information we learn to improve our marketing choices. We also take social monitoring a step further to better comprehend brand-focused social media interactions."
@@ -94,7 +94,7 @@ export const services5 = [
 export const services6 = [
     {
         id: 6,
-        imagePath: "../src/assets/service6.svg",
+        imagePath: "../public/service6.svg",
         title: "Content",
         title2: "Development",
         info: "By conducting research, writing, compiling, arranging, and editing information for publishing, we assist you in developing content. As a result of these released materials, you will increase the audience's loyalty and trust since they will regard you as an authority."
@@ -103,15 +103,15 @@ export const services6 = [
 
 export const works = [
     {
-        imagePath: "../src/assets/works1.svg",
+        imagePath: "../public/works1.svg",
         info: "Lorem Ipsum is simply dummy text of the printing."
     },
     {
-        imagePath: "../src/assets/works2.svg",
+        imagePath: "../public/works2.svg",
         info: "Lorem Ipsum is simply dummy text of the printing."
     },
     {
-        imagePath: "../src/assets/works3.svg",
+        imagePath: "../public/works3.svg",
         info: "Lorem Ipsum is simply dummy text of the printing."
     }
 ]
